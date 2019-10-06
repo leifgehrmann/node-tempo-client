6d69c72ee5f49967a841c111b82d059bffd1c568
Leif Gehrmann <leif.gehrmann@gmail.com>
2019-10-06 14:25:06 +0100

    First untested implementation

--- src/__tests__/tempo.test.ts
+++ src/__tests__/tempo.test.ts
@@ -1,4 +1,39 @@
-import { WorkInProgress } from '../tempo';
-test('My WIP', () => {
-  expect(WorkInProgress()).toBe('WIP');
+import TempoApi from '../tempo';
+
+
+
+
+
+function getOptions(options?: any) {
+  const actualOptions = options || {};
+  return {
+    apiVersion: actualOptions.apiVersion || '3.0',
+    bearerToken: actualOptions.bearer || 'sometoken',
+    ca: actualOptions.ca || null,
+    host: actualOptions.host || 'tempo.somehost.com',
+    intermediatePath: actualOptions.intermediatePath,
+    port: actualOptions.port || '8080',
+    protocol: actualOptions.protocol || 'http',
+    request: actualOptions.request,
+    strictSSL: actualOptions.hasOwnProperty('strictSSL')
+      ? actualOptions.strictSSL
+      : true,
+    timeout: actualOptions.timeout || null
+  };
+}
+
+describe('Jira API Tests', () => {
+  describe('Constructor Tests', () => {
+    it('Constructor functions properly', () => {
+      const jira = new TempoApi(getOptions());
+
+      expect(jira.protocol).toEqual('http');
+      expect(jira.host).toEqual('tempo.somehost.com');
+      expect(jira.port).toEqual('8080');
+      expect(jira.baseOptions.auth.user).toEqual('');
+      expect(jira.baseOptions.auth.pass).toEqual('');
+      expect(jira.baseOptions.auth.bearer).toEqual('sometoken');
+      expect(jira.apiVersion).toEqual('3.0');
+    });
+  });
 });
--- src/tempo.ts
+++ src/tempo.ts
@@ -1,1 +1,144 @@
-export const WorkInProgress = () => 'WIP';
+import * as request from 'request-promise';
+import * as url from 'url';
+
+interface ITempoApiOptions {
+  port: string;
+  request?: request.RequestPromiseAPI;
+  timeout?: number;
+  protocol: string;
+  host: string;
+  apiVersion: string;
+  intermediatePath?: string;
+  bearerToken?: string;
+  strictSSL?: boolean;
+}
+
+interface IStringMap {
+  [s: string]: string;
+}
+
+interface IPaginationFromQueryOptions extends IStringMap {
+  offset: string;
+  limit: string;
+}
+
+interface IUpdatedFromQueryOptions extends IStringMap {
+  updatedFrom: string;
+}
+
+interface IDateRangeQueryOptions extends IStringMap {
+  from: string;
+  to: string;
+}
+
+interface IRequestHeaderOptions {
+  method?: string;
+}
+
+interface IUriOptions {
+  pathname: string;
+  query: { [key: string]: string };
+  intermediatePath?: string;
+}
+
+export default class TempoApi {
+  public readonly protocol: string;
+  public readonly port?: string;
+  public readonly host: string;
+  public readonly apiVersion: string;
+  public readonly bearerToken?: string;
+  public readonly intermediatePath: string;
+  public readonly strictSSL: boolean | undefined;
+  public readonly request: request.RequestPromiseAPI;
+  public readonly baseOptions: {
+    timeout?: number;
+    auth?: any;
+  };
+
+  constructor(options: ITempoApiOptions) {
+    this.protocol = options.protocol || 'https';
+    this.intermediatePath = options.intermediatePath || 'core';
+    this.strictSSL = options.hasOwnProperty('strictSSL')
+      ? options.strictSSL
+      : true;
+    this.port = options.port;
+    this.host = options.host;
+    this.apiVersion = options.apiVersion || '3';
+    this.bearerToken = options.bearerToken;
+    this.request = options.request || request; // To mock requests
+    this.baseOptions = {};
+
+    if (this.bearerToken) {
+      this.baseOptions.auth = {
+        bearer: this.bearerToken,
+        pass: '',
+        sendImmediately: true,
+        user: ''
+      };
+    }
+
+    if (options.timeout) {
+      this.baseOptions.timeout = options.timeout;
+    }
+  }
+
+  public getWorklogsForUser(
+    accountId: string,
+    options: IDateRangeQueryOptions &
+      IUpdatedFromQueryOptions &
+      IPaginationFromQueryOptions
+  ) {
+    return this.doRequest(
+      this.makeRequestHeader(
+        this.makeUri({
+          pathname: `/worklogs/user/${accountId}`,
+          query: options
+        })
+      )
+    );
+  }
+
+  private async doRequest(requestOptions: any) {
+    const options = {
+      ...this.baseOptions,
+      ...requestOptions
+    };
+
+    const response = await this.request(options);
+
+    if (response) {
+      if (
+        Array.isArray(response.errorMessages) &&
+        response.errorMessages.length > 0
+      ) {
+        throw new Error(response.errorMessages.join(', '));
+      }
+    }
+
+    return response;
+  }
+
+  private makeRequestHeader(uri: string, options: IRequestHeaderOptions = {}) {
+    return {
+      json: true,
+      method: options.method || 'GET',
+      rejectUnauthorized: this.strictSSL,
+      uri,
+      ...options
+    };
+  }
+
+  private makeUri({ pathname, query, intermediatePath }: IUriOptions) {
+    const intermediateToUse = this.intermediatePath || intermediatePath;
+    const tempPath = intermediateToUse || `/core/${this.apiVersion}`;
+    const uri = url.format({
+      hostname: this.host,
+      pathname: `${tempPath}${pathname}`,
+      port: this.port,
+      protocol: this.protocol,
+      query
+    });
+
+    return decodeURIComponent(uri);
+  }
+}
