9cca52bca65e0d984e4ed702d5038fe7101ff0d5
Leif Gehrmann <leif.gehrmann@gmail.com>
2019-11-02 21:13:41 +0000

    Added 💯% code coverage for RequestHeader

--- tests/request/handler.test.ts
+++ tests/request/handler.test.ts
@@ -4,16 +4,17 @@ function getOptions(options?: any) {
   const actualOptions = options || {};
   return {
     apiVersion: actualOptions.apiVersion || '3',
-    bearerToken: actualOptions.bearer || 'sometoken',
-    ca: actualOptions.ca || null,
+    bearerToken: actualOptions.hasOwnProperty('bearerToken')
+      ? actualOptions.bearerToken
+      : 'someToken',
     host: actualOptions.host || 'tempo.somehost.com',
     intermediatePath: actualOptions.intermediatePath,
     port: actualOptions.port || '8080',
-    protocol: actualOptions.protocol || 'http',
+    protocol: actualOptions.protocol,
     request: actualOptions.request,
     strictSSL: actualOptions.hasOwnProperty('strictSSL')
       ? actualOptions.strictSSL
-      : true,
+      : undefined,
     timeout: actualOptions.timeout || null
   };
 }
@@ -21,20 +22,176 @@ function getOptions(options?: any) {
 describe('TempoAi', () => {
   describe('Constructor Tests', () => {
     it('Constructor functions properly', () => {
-      const tempo = new requestHandler(
+      const handler = new requestHandler(
         getOptions({
           timeout: 3000
         })
       );
 
-      expect(tempo.protocol).toEqual('http');
-      expect(tempo.host).toEqual('tempo.somehost.com');
-      expect(tempo.port).toEqual('8080');
-      expect(tempo.baseOptions.auth.user).toEqual('');
-      expect(tempo.baseOptions.auth.pass).toEqual('');
-      expect(tempo.baseOptions.auth.bearer).toEqual('sometoken');
-      expect(tempo.baseOptions.timeout).toEqual(3000);
-      expect(tempo.apiVersion).toEqual('3');
+      expect(handler.host).toEqual('tempo.somehost.com');
+      expect(handler.port).toEqual('8080');
+      expect(handler.baseOptions.auth!.user).toEqual('');
+      expect(handler.baseOptions.auth!.pass).toEqual('');
+      expect(handler.baseOptions.auth!.bearer).toEqual('someToken');
+      expect(handler.baseOptions.auth!.sendImmediately).toEqual(true);
+      expect(handler.baseOptions.timeout).toEqual(3000);
+      expect(handler.apiVersion).toEqual('3');
+
+      // Defaults are set to expected values
+      expect(handler.protocol).toEqual('https');
+      expect(handler.strictSSL).toEqual(true);
+
+      // strictSSL can be turned off
+      const handlerWithNoStrictSSL = new requestHandler(
+        getOptions({ strictSSL: false })
+      );
+      expect(handlerWithNoStrictSSL.strictSSL).toEqual(false);
+
+      // Not having a bearerToken means no base auth headers are set
+      const handlerWithNoBearerToken = new requestHandler(
+        getOptions({ bearerToken: undefined })
+      );
+      expect(handlerWithNoBearerToken.baseOptions).toEqual({});
+      expect(handlerWithNoBearerToken.baseOptions.auth).toEqual(undefined);
+    });
+  });
+
+  describe('doRequest', () => {
+    it('Merges some constructor options with requestHeader options', async () => {
+      const handler = new requestHandler(
+        getOptions({
+          timeout: 4000,
+          bearerToken: 'myFakeToken',
+          request: async (requestOptions: any) => {
+            return requestOptions;
+          }
+        })
+      );
+
+      const mockResponse = await handler.doRequest({
+        uri: 'https://example.com'
+      });
+      expect(mockResponse.uri).toEqual('https://example.com');
+      expect(mockResponse.auth!.user).toEqual('');
+      expect(mockResponse.auth!.pass).toEqual('');
+      expect(mockResponse.auth!.bearer).toEqual('myFakeToken');
+      expect(mockResponse.timeout).toEqual(4000);
+    });
+
+    it('Returns falsey response if falsey', async () => {
+      const handler = new requestHandler(
+        getOptions({
+          request: async () => undefined
+        })
+      );
+
+      const mockResponse = await handler.doRequest({
+        uri: 'https://example.com'
+      });
+      expect(mockResponse).toEqual(undefined);
+    });
+
+    it('Throws no error if error messages length is 0', async () => {
+      const handler = new requestHandler(
+        getOptions({
+          request: async () => {
+            return {
+              errorMessages: []
+            };
+          }
+        })
+      );
+
+      const mockResponse = await handler.doRequest({
+        uri: 'https://example.com'
+      });
+      expect(mockResponse).toEqual({ errorMessages: [] });
+    });
+
+    it('Throws error if error messages length is at least 1', async () => {
+      expect.assertions(1);
+      const handler = new requestHandler(
+        getOptions({
+          request: async () => {
+            return {
+              errorMessages: ['Something went wrong!']
+            };
+          }
+        })
+      );
+
+      try {
+        await handler.doRequest({ uri: 'https://example.com' });
+      } catch (e) {
+        expect(e.message).toMatch('Something went wrong!');
+      }
+    });
+  });
+
+  describe('makeRequestHeader', () => {
+    it('Sets correct default values', () => {
+      const handler = new requestHandler(getOptions());
+
+      const requestHeader = handler.makeRequestHeader('https://example.com');
+      expect(requestHeader.json).toEqual(true);
+      expect(requestHeader.method).toEqual('GET');
+      expect(requestHeader.rejectUnauthorized).toEqual(true);
+      expect(requestHeader.uri).toEqual('https://example.com');
+    });
+
+    it('Sets rejectUnauthorized correctly based on constructor options', () => {
+      const handler = new requestHandler(getOptions({ strictSSL: false }));
+      const requestHeader = handler.makeRequestHeader('https://example.com');
+      expect(requestHeader.rejectUnauthorized).toEqual(false);
+    });
+
+    it('Sets method based on options', () => {
+      const handler = new requestHandler(getOptions());
+      const requestHeader = handler.makeRequestHeader('https://example.com', {
+        method: 'DELETE'
+      });
+      expect(requestHeader.method).toEqual('DELETE');
+    });
+
+    it('Sets other request options', () => {
+      const handler = new requestHandler(getOptions());
+      const requestHeader = handler.makeRequestHeader('https://example.com', {
+        encoding: 'customEncodingValue'
+      });
+      expect(requestHeader.encoding).toEqual('customEncodingValue');
+    });
+  });
+
+  describe('makeUri', () => {
+    it('Sets correct default values', () => {
+      const handler = new requestHandler(getOptions());
+      const uri = handler.makeUri({
+        pathname: '/collection/selector',
+        query: { queryKey: 'queryValue' }
+      });
+      expect(uri).toEqual(
+        'https://tempo.somehost.com:8080/core/3/collection/selector?queryKey=queryValue'
+      );
+    });
+
+    it('Sets intermediate path', () => {
+      const handler = new requestHandler(getOptions());
+      const uri = handler.makeUri({
+        pathname: '/a/b',
+        intermediatePath: '/myCustomPath'
+      });
+      expect(uri).toEqual('https://tempo.somehost.com:8080/myCustomPath/a/b');
+    });
+
+    it('URI encodes parameters', () => {
+      const handler = new requestHandler(getOptions());
+      const uri = handler.makeUri({
+        pathname: '/a/b',
+        query: { x: 'hello world' } // We do not want spaces to be encoded as %20
+      });
+      expect(uri).toEqual(
+        'https://tempo.somehost.com:8080/core/3/a/b?x=hello world'
+      );
     });
   });
 });
