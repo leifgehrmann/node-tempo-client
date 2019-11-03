d571a28684b259d5cf399d1b90be491ea9659c96
Leif Gehrmann <leif.gehrmann@gmail.com>
2019-11-03 14:06:57 +0000

    Added types for userSchedule

--- src/collections/userSchedule.ts
+++ src/collections/userSchedule.ts
@@ -1,28 +1,38 @@
 import * as queryOptions from '../queryOptions';
 import Collection from './collection';
 
+interface IDayScheduleResponseObject {
+  date: string;
+  requiredSeconds: number;
+  type: string;
+  holiday?: {
+    name: string;
+    description?: string;
+    durationSeconds: number;
+  }
+}
+
+interface IDaySchedulesResponseObject {
+  self: string;
+  metadata: {
+    count: string;
+  }
+  results: IDayScheduleResponseObject[]
+}
+
 export default class UserSchedule extends Collection {
-  public async get(options?: queryOptions.IDateRange) {
-    return await this.requestHandler.doRequest(
-      this.requestHandler.makeRequestHeader(
-        this.requestHandler.makeUri({
-          pathname: `/user-schedule`
-        })
-      )
-    );
+  public async get(options?: queryOptions.IDateRange): Promise<IDaySchedulesResponseObject> {
+    return await this.createAndSendRequest(`/user-schedule`, {
+      query: options
+    });
   }
 
   public async getForUser(
     accountId: string,
     options?: queryOptions.IDateRange
-  ) {
-    return await this.requestHandler.doRequest(
-      this.requestHandler.makeRequestHeader(
-        this.requestHandler.makeUri({
-          pathname: `/user-schedule/${accountId}`,
-          query: options
-        })
-      )
-    );
+  ): Promise<IDaySchedulesResponseObject> {
+    return await this.createAndSendRequest(`/user-schedule/${accountId}`, {
+      query: options
+    });
   }
 }
