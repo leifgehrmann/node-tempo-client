8c8d0dfc739cba682a93b103158c92d2350cfad7
Leif Gehrmann <leif.gehrmann@gmail.com>
2019-11-03 14:07:14 +0000

    Fix linting

--- src/collections/userSchedule.ts
+++ src/collections/userSchedule.ts
@@ -9,19 +9,21 @@ interface IDayScheduleResponseObject {
     name: string;
     description?: string;
     durationSeconds: number;
-  }
+  };
 }
 
 interface IDaySchedulesResponseObject {
   self: string;
   metadata: {
     count: string;
-  }
-  results: IDayScheduleResponseObject[]
+  };
+  results: IDayScheduleResponseObject[];
 }
 
 export default class UserSchedule extends Collection {
-  public async get(options?: queryOptions.IDateRange): Promise<IDaySchedulesResponseObject> {
+  public async get(
+    options?: queryOptions.IDateRange
+  ): Promise<IDaySchedulesResponseObject> {
     return await this.createAndSendRequest(`/user-schedule`, {
       query: options
     });
