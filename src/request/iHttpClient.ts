eb11a8bc393af35b61fac4d701aa5382cddae82a
Leif Gehrmann <leif.gehrmann@gmail.com>
2019-11-15 22:31:32 +0000

    Clarify responsibilities of iHttpClient

--- src/request/iHttpClient.ts
+++ src/request/iHttpClient.ts
@@ -2,5 +2,5 @@ import { IRequestConfig } from './iRequestConfig';
 
 // The HTTP Client must behave the following way:
 // * Throw an exception if the response code is not between 200 and 300
-// * Return an JavaScript object (i.e. not a response string)
+// * Return a JavaScript object of the server's JSON response
 export type IHttpClient = (arg1: IRequestConfig) => Promise<any>;
